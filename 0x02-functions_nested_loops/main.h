82dee0cf58644b13e62cf91d25eb84a82a05f6a7
Livingstone amao <amaostone@gmail.com>
2022-03-15 15:16:19 -0700

    loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,8 +8,9 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_sign(int n);
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-void time_table(int n);
+int add(int, int);
+void print_to_98(int n);
 #endif
